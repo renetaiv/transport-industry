aec5c6fc6013a02c9b02a3a52f2cbe4bd4e5d079
Reneta Ivanova <reneta.iv12@gmail.com>
2021-03-22 13:08:16 +0200

    Add new functionalities to Travelable

--- src/main/java/main/Travelable.java
+++ src/main/java/main/Travelable.java
@@ -1,4 +1,18 @@
 package main;
 
+import java.util.Set;
+
 public interface Travelable {
+
+    default void addPassenger(Person person, Set<Person> passengers, int maxCapacity){
+        if (passengers.size() < maxCapacity) {
+            passengers.add(person);
+        }
+    }
+
+    default void showPassengers(Set<Person> passengers) {
+        for (Person passenger : passengers) {
+            System.out.println(passenger);
+        }
+    }
 }
